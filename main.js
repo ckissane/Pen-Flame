12a92b417cca81cf65a23eacd7bfef6da9e7ec67
coler706 <coler706@gmail.com>
2016-03-18 17:12:48 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ var c = $("#c")[0];
 var ctx = c.getContext("2d");
 var arr = [];
 var arrTemp = [];
-var w = 500;
-var h = 300;
+var w = document.body.clientWidth - 4;
+var h = document.body.clientHeight - 4;
+var fps = 60,
+    step = 1 / fps,
+    dt = 0,
+    now, last = timestamp();
 var mouse = {
     x: 0,
     y: 0
@@ -12,95 +16,112 @@ var pressed = false;
 
 c.width = w;
 c.height = h;
+var options = {
+    size: 3,
+    strength: 2,
+    expand: 1,
+    speed: 8,
+    squaresize: 5
+};
+function bound(x, min, max) {
+    return Math.max(min, Math.min(max, x));
+}
 
+
+function timestamp() {
+    if (window.performance && window.performance.now)
+        return window.performance.now();
+    else
+        return new Date().getTime();
+}
 for (var r = 0; r < w - 1; r++) {
     arr[r] = [];
     for (var c = 0; c < h - 1; c++) {
         arr[r][c] = [];
     }
 }
-/*for(var i=0;i<100000;i++){
-    arr[250][250].push("red");
-}*/
 var nodes = [];
-var colors = ["orange", "red"];
+var colors = ["hsla(20,100%,50%,0.5)", "hsla(0,100%,50%,0.2)"];
+
+function node(x, y) {
+    this.x = x;
+    this.y = y;
+    this.color = "hsla(20,100%,50%,0.5)";
+    return this;
+}
 
 function tick() {
-
-    for (var i = 0; i < 50; i += 1) {
-        arr[298][i % 25 + 225].push(colors[Math.floor(Math.random() * colors.length)]);
-    }
     if (pressed) {
-        for (var y = -5; y <= 5; y++) {
-            for (var x = -5; x <= 5; x++) {
-                arr[Math.max(Math.min(w - 1, Math.floor(mouse.y + y)), 0)][Math.max(Math.min(h - 1, Math.floor(mouse.x + x)), 0)].push(colors[Math.floor(Math.random() * colors.length)]);
-            }
-        }
-    }
-    arrTemp = [];
-    for (var r = 0; r < w; r++) {
-        arrTemp[r] = [];
-        for (var c = 0; c < h; c++) {
-            arrTemp[r][c] = [];
-        }
-    }
-    for (var r = 0; r < w - 1; r++) {
-        for (var c = 0; c < h - 1; c++) {
-            nodes = arr[r][c];
-
-            for (var i = 0; i < nodes.length; i++) {
-                var choice = Math.floor(Math.random() * 5);
-                if (Math.floor(Math.random() * 100) != 10) {
-
-
-                    if (choice + "" == "0") {
-                        if (r > 0) {
-                            arrTemp[r - 1][c].push(nodes[i]);
-                        }
-                    } else if (choice + "" == "1") {
-                        if (c < h - 1) {
-                            arrTemp[r][c + 1].push(nodes[i]);
-                        }
-                    } else if (choice == 2) {
-                        /*if (r < w - 1) {
-                        arrTemp[r + 1][c].push(nodes[i]);
-                    }*/
-                        if (r > 0) {
-                            arrTemp[r - 1][c].push(nodes[i]);
-                        }
-                    } else if (choice == 3) {
-                        if (c > 0) {
-                            arrTemp[r][c - 1].push(nodes[i]);
-                        }
-                    } else {
-
-                        arrTemp[r][c].push(nodes[i]);
-
-                    }
+        for (var i = 0; i < options.strength; i++) {
+            for (var y = -1; y <= options.size * options.expand; y += options.expand) {
+                for (var x = -1; x <= options.size * options.expand; x += options.expand) {
+                    nodes.push(new node(Math.max(Math.min(w - 1, Math.floor(mouse.x + x - options.size * options.expand / 2)), 0), Math.max(Math.min(h - 1, Math.floor(mouse.y + y - options.size * options.expand / 2)), 0)));
                 }
             }
         }
     }
-    for (var r = 0; r < w; r++) {
-        arr[r] = arrTemp[r];
+    arrTemp = [];
+
+    for (var i = 0; i < nodes.length; i++) {
+        var nod = nodes[i];
+        var r = nod.y;
+        var c = nod.x;
+        if (Math.floor(Math.random() * 200) == 5) {
+            if (nodes[i].color == "hsla(20,100%,50%,0.5)") {
+                nodes[i].color = "hsla(0,100%,50%,0.2)";
+            }
+        }
+        if (Math.floor(Math.random() * 400) == 5) {
+            if (nodes[i].color == "hsla(0,100%,50%,0.2)") {
+                nodes[i].color = "hsla(20,100%,50%,0.1)";
+            }
+        }
+
+        if (Math.floor(Math.random() * 300) == 5) {
+            if (nodes[i].color == "hsla(0,100%,50%,0.2)") {
+                nodes[i].color = "hsla(20,100%,50%,0.5)";
+            }
+        }
+        var choice = Math.floor(Math.random() * 5);
+        if (Math.floor(Math.random() * 40) != 10) {
+            if (choice === 0 && r > 0) {
+                nod.y -= options.speed;
+            } else if (choice == 1 && c < w - 1) {
+                nod.x += options.speed;
+            } else if (choice == 2 && r > 0) {
+                nod.y -= options.speed;
+            } else if (choice == 3 && c > 0) {
+                nod.x -= options.speed;
+            }
+        } else {
+            nodes.splice(i, 1);
+            i--;
+        }
     }
 }
 
 function draw() {
-    ctx.clearRect(0, 0, w, h);
-    for (var r = 0; r < w - 1; r++) {
-        for (var c = 0; c < h - 1; c++) {
-            nodes = arr[r][c];
-            if (nodes.length > 0) {
-                ctx.beginPath();
-                ctx.fillStyle = "hsl(0,90%," + Math.min(100, nodes.length * 10 - 10) + "%)";
-                ctx.fillRect(c, r, 1, 1);
-                ctx.fill();
+    if (w != document.body.clientWidth - 4 || h != document.body.clientHeight - 4) {
+        w = document.body.clientWidth - 4;
+        h = document.body.clientHeight - 4;
 
-            }
-
-        }
+        c.width = w;
+        c.height = h;
     }
+    ctx.clearRect(0, 0, w, h);
+    ctx.globalCompositeOperation = "screen";
+    for (var c = 0; c < nodes.length; c++) {
+        ctx.beginPath();
+        ctx.fillStyle = nodes[c].color;
+        ctx.arc(nodes[c].x, nodes[c].y, options.squaresize, 0,Math.PI*2,false);
+        ctx.fill();
+    }
+    
+    /*ctx.globalCompositeOperation = "difference";
+    ctx.beginPath();
+        ctx.fillStyle = "black";
+        ctx.fillRect(0,0,w,h);
+        ctx.fill();*/
 }
 document.body.onmousedown = function(event) {
     mouse = {
@@ -122,5 +143,18 @@ document.body.onmousemove = function(event) {
         y: event.clientY
     };
 };
-window.setInterval(tick, 1);
-window.setInterval(draw, 100);
+//window.setInterval(tick, 1);
+//window.setInterval(draw, 10);
+function frame() {
+    now = timestamp();
+    dt = dt + Math.min(1, (now - last) / 1000);
+    while (dt > step) {
+        dt = dt - step;
+        tick();
+    }
+    draw();
+    last = now;
+    requestAnimationFrame(frame, c);
+}
+
+frame(); // start the first frame
